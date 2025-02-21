e7bcd7fe83c7e73b4f2496a7ebb9250e5988508b
PRASHANTHCT27 <prashanthpilinja@gmail.com>
2025-02-21 11:05:09 +0530

    first commit

---
+++ Demo21.java
@@ -0,0 +1,16 @@
+class Demo21
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(20+20);
+		System.out.println(+"THE VALUE IS" +20);
+		System.out.println(20+"IS THE VALUE");
+		
+		System.out.println("THE VALUE IS"+20+20);
+		System.out.println(20+20+ "IS THE VALUE");
+		System.out.println("THE VALUE IS"+(20+20));
+		System.out.println("THE VALUE IS"+(1/2));
+
+		System.out.println("THE VALUE IS"+(1/2.0));
+	}
+}
